ae28b36fcfe99200e4e0221e9735787ef3279236
sdn311 <35548813+sdn311@users.noreply.github.com>
2018-02-07 19:08:54 -0700

    Update chaco_plain.js

--- chaco_plain.js
+++ chaco_plain.js
@@ -1,9 +1,9 @@
 function initialize() {
     var Chaco = new google.maps.LatLng(-23.693853,-59.489595)
     var mapOptions = {
-      zoom: 8,
-      center: Chaco,
-      mapTypeId: google.maps.MaptypeId.SATELLITE
+        zoom: 8,
+        center: Chaco,
+        mapTypeId: google.maps.MaptypeId.SATELLITE
     };
     var map = new google.maps.Map(
         document.getElementById("map_canvas"),
