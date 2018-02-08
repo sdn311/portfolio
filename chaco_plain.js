ceb18460f92da81faabbfb42033ab402970987c3
sdn311 <35548813+sdn311@users.noreply.github.com>
2018-02-07 18:50:28 -0700

    Update chaco_plain.js

--- chaco_plain.js
+++ chaco_plain.js
@@ -1,6 +1,6 @@
-function initialize () {
-  var classroom = new google.maps.LatLng(-23.693853,-59.489595)
-  var mapOptions = {
+function initialize() {
+    var classroom = new google.maps.LatLng(-23.693853,-59.489595)
+    var mapOptions = {
       zoom: 8,
       center: classroom,
       mapTypeId:google.maps.MaptypeId.SATELLITE
