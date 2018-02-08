582a15c805661c80b60c4c39e808cb1b194c2602
sdn311 <35548813+sdn311@users.noreply.github.com>
2018-02-07 19:07:51 -0700

    Update chaco_plain.js

--- chaco_plain.js
+++ chaco_plain.js
@@ -1,11 +1,11 @@
 function initialize() {
-    var classroom = new google.maps.LatLng(-23.693853,-59.489595)
+    var Chaco = new google.maps.LatLng(-23.693853,-59.489595)
     var mapOptions = {
       zoom: 8,
-      center: classroom,
-      mapTypeId:google.maps.MaptypeId.SATELLITE
-  };
-      var map = new google.maps.Map(
-      document.getElementById("map_canvas"),
-      mapOptions);
+      center: Chaco,
+      mapTypeId: google.maps.MaptypeId.SATELLITE
+    };
+    var map = new google.maps.Map(
+        document.getElementById("map_canvas"),
+        mapOptions);
 }
