8a4d122493567ea5f3c6bee238d910f37afe7e26
sdn311 <35548813+sdn311@users.noreply.github.com>
2018-02-21 17:03:06 -0700

    Update chaco_plain_styled.js

--- chaco_plain_styled.js
+++ chaco_plain_styled.js
@@ -331,9 +331,10 @@ function initialize() {
     ]
   }
 ]
+
+};
           
     var map = new google.maps.Map(
         document.getElementById("map_canvas"),
         myOptions);
-          }
-        }
+}
