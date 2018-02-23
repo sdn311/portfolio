e9a4246e808f3c825be67d33d98967e950cb0f5d
sdn311 <35548813+sdn311@users.noreply.github.com>
2018-02-22 17:46:07 -0700

    Update chaco_plain_deepdive.js

--- chaco_plain_deepdive.js
+++ chaco_plain_deepdive.js
@@ -337,11 +337,22 @@ function initialize() {
         mapOptions);
 
     var triangleCoords = [
-    {lat: -21.241547, lng: -63.607770},
-    {lat: -19.653194, lng: -58.677126},
-    {lat: -25.442586, lng: -58.592972},
-    {lat: -21.241547, lng: -63.607770}
+        {lat: -21.241547, lng: -63.607770},
+        {lat: -19.653194, lng: -58.677126},
+        {lat: -22.092490, lng: -58.004426},
+        {lat: -25.442586, lng: -58.592972},
+        {lat: -21.241547, lng: -63.607770}
   ];
+      
+    var megaFan = new google.maps.Polygon({
+        paths: triangleCoords,
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35
+    });
+    megaFan.setMap(map);
 
 }
 
