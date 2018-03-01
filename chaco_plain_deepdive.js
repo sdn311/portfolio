19258c2231e4dc20d5fcbd0aaeb84a4aec770d3f
sdn311 <35548813+sdn311@users.noreply.github.com>
2018-02-28 17:38:37 -0700

    Update chaco_plain_deepdive.js

--- chaco_plain_deepdive.js
+++ chaco_plain_deepdive.js
@@ -332,28 +332,6 @@ function initialize() {
   }
 ]
     };    
-    var map = new google.maps.Map(
-        document.getElementById("map_canvas"),
-        mapOptions);
 
-    var triangleCoords = [
-        {lat: -21.241547, lng: -63.607770},
-        {lat: -19.653194, lng: -58.677126},
-        {lat: -22.092490, lng: -58.004426},
-        {lat: -25.442586, lng: -58.592972},
-        {lat: -21.241547, lng: -63.607770}
-  ];
-      
-    var megaFan = new google.maps.Polygon({
-        paths: triangleCoords,
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: '#FF0000',
-        fillOpacity: 0.35
-    });
-    megaFan.setMap(map);
-
-}
 
     
