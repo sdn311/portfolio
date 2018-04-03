6ad16b6a11c54273d8c110d7e14d173aadad79f4
sdn311 <35548813+sdn311@users.noreply.github.com>
2018-04-02 19:14:21 -0600

    Update openlayers.js

--- openlayers.js
+++ openlayers.js
@@ -1,1 +1,12 @@
-
+var myMap = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([]),
+      zoom:
+      })
+    });
