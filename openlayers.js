c505dfe911b48659bdc0c4ff34f93fcda70c0b8d
sdn311 <35548813+sdn311@users.noreply.github.com>
2018-04-02 19:16:21 -0600

    Update openlayers.js

--- openlayers.js
+++ openlayers.js
@@ -1,12 +1,40 @@
-var myMap = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([]),
-      zoom:
-      })
-    });
+      var openCycleMapLayer = new ol.layer.Tile({
+        source: new ol.source.OSM({
+          attributions: [
+            'All maps © <a href="https://www.opencyclemap.org/">OpenCycleMap</a>',
+            ol.source.OSM.ATTRIBUTION
+          ],
+          url: 'https://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png' +
+              '?apikey=b09b6ad078bd48148c1ed08beebda393'
+        })
+      });
+
+      var openSeaMapLayer = new ol.layer.Tile({
+        source: new ol.source.OSM({
+          attributions: [
+            'All maps © <a href="http://www.openseamap.org/">OpenSeaMap</a>',
+            ol.source.OSM.ATTRIBUTION
+          ],
+          opaque: false,
+          url: 'https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png'
+        })
+      });
+      
+      var map = new ol.Map({
+        layers: [
+          openCycleMapLayer,
+          openSeaMapLayer
+        ],
+        target: 'map',
+        controls: ol.control.defaults({
+          attributionOptions: {
+            collapsible: false
+          }
+        }),
+        view: new ol.View({
+          maxZoom: 18,
+          center: ol.proj.fromLonLat([-28.332091,38.469306]),
+          zoom: 10
+        })
+      });
+    
